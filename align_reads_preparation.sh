5038ccb6b189039a1e2110a8654e291ad706c3c6
Anne-Sophie Benoiston <AnneSoBen@users.noreply.github.com>
2022-05-15 17:52:07 +0200

    Create align_reads_preparation.sh

---
+++ align_reads_preparation.sh
@@ -0,0 +1,9 @@
+cd ~/work/bash_slurm_training/slurm
+mkdir metabarcoding
+cd metabarcoding
+mkdir analysis results # organize the folder
+cd analysis
+wget https://raw.githubusercontent.com/Imogen-D/Bases_De_Bash_Workshop/main/align_reads_basic.sh
+wget https://raw.githubusercontent.com/Imogen-D/Bases_De_Bash_Workshop/main/align_reads_intermediate.sh
+wget https://raw.githubusercontent.com/Imogen-D/Bases_De_Bash_Workshop/main/align_reads_difficult.sh
+cd ..
