789ae7b8934969497cfad5caa79f95a8b5410cd7
Anne-Sophie Benoiston <AnneSoBen@users.noreply.github.com>
2022-05-15 17:21:16 +0200

    Create align_reads_basic.sh

---
+++ align_reads_basic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load bioinfo/obitools-v1.2.11
+
+# align reads that have a minimum alignment score of 40 + concatenate other reads:
+srun illuminapairedend --score-min=40 -r ../../data/MiSeq_SOP/fastq_files/F3D0_S188_L001_R1_001.fastq ../../data/MiSeq_SOP/fastq_files/F3D0_S188_L001_R2_001.fastq > results/F3D0_S188_L001_R1R2_001.fastq
+# remove unaligned reads:
+srun obigrep -p 'mode!="joined"' results/F3D0_S188_L001_R1R2_001.fastq > results/F3D0_S188_L001_R1R2_001.ali.fastq
+# compute stats:
+srun obistat -c score results/F3D0_S188_L001_R1R2_001.ali.fastq > results/F3D0_S188_stats.txt
