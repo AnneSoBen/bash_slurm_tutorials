9f4b64db41015ccfe7b558abde67eaf5728c5cc1
Imogen Dumville <57735780+Imogen-D@users.noreply.github.com>
2022-05-16 10:54:32 +0200

    Update Ypestis_align.sh

--- Ypestis_align.sh
+++ Ypestis_align.sh
@@ -16,19 +16,19 @@ module load bioinfo/bcftools-1.14
 #this is the alignment function, look at the documentation for bwa for more information and parameters
 echo indexing and aligning
 bwa index analyses/$genome #bwa requires the genome to be indexed
-bwa mem $genome $fastq_file > ${name}_aligned.sam
+bwa mem $genome $fastq_file > results/${name}_aligned.sam
 
 #now we use 'samtools' a common tool for aligned sequence data, but first we convert our 'sam' to a 'bam'
 echo coverting sam to bam
-samtools view -S -b ${name}_aligned.sam > ${name}_aligned.bam
+samtools view -S -b results/${name}_aligned.sam > results/${name}_aligned.bam
 
 #we have to sort the alignment
 echo sorting the alignment
-samtools sort -o ${name}_aligned_sorted.bam ${name}_aligned.bam 
+samtools sort -o results/${name}_aligned_sorted.bam results/${name}_aligned.bam 
 
 #we can look at a histogram of coverage
 echo making the coverage table
-samtools coverage -m -o ${name}_aligned_sorted_coveragetables.txt ${name}_aligned_sorted.bam
+samtools coverage -m -o results/${name}_aligned_sorted_coveragetables.txt results/${name}_aligned_sorted.bam
 
 #and we can call SNPs, which is a bit more complicated than it needs to be...
 
@@ -38,7 +38,7 @@ gunzip -c $genome | bgzip > $genome.bgz
 
 #then we run samtools mpileup, which Collects summary information in the input BAMs, computes the likelihood of data given each possible genotype and stores the likelihoods in the BCF format, which then enables the snps to be called by bcftools 
 echo snp calling
-bcftools mpileup -Ou -f $genome.bgz ${name}_aligned_sorted.bam | bcftools call -mv -Ov -o ${name}_aligned_sorted_snps.vcf
+bcftools mpileup -Ou -f $genome.bgz results/${name}_aligned_sorted.bam | bcftools call -mv -Ov -o results/${name}_aligned_sorted_snps.vcf
 
 echo the script is done
 
