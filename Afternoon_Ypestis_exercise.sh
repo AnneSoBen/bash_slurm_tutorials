d0841d5c48dd92a695af6514ed79fbd3631085ef
Imogen Dumville <57735780+Imogen-D@users.noreply.github.com>
2022-05-16 15:05:27 +0200

    Update Afternoon_Ypestis_exercise.sh

--- Afternoon_Ypestis_exercise.sh
+++ Afternoon_Ypestis_exercise.sh
@@ -35,8 +35,8 @@ chmod +x analyses/Ypestis_align.sh #make the script executable
 #lets give our output a name with a variable
 output_name=Ypestis
 
-#these are very small files so we manage with only requesting 1 cpu and 4G memory
-sbatch --cpus-per-task=1 --mem=4G  -J align.$output_name -o results/align.${output_name}.log analyses/Ypestis_align.sh analyses/SRR18369392.fastq analyses/GCF_015336265.1_ASM1533626v1_genomic.fna.gz  $output_name #run the script, giving it the genome and the fastq file
+#these are very small files so we manage with only requesting 1 cpu and 1G memory
+sbatch --cpus-per-task=1 --mem=1G  -J align.$output_name -o results/align.${output_name}.log analyses/Ypestis_align.sh analyses/SRR18369392.fastq analyses/GCF_015336265.1_ASM1533626v1_genomic.fna.gz  $output_name #run the script, giving it the genome and the fastq file
 
 #check if the job is running, change $user to your username
 squeue -u $user -o "%.15i %.30j %.8u %.2t %.10M %.6D %.6C %.6m %R"
