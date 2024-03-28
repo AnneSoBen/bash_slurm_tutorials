dde2404f6c9467c6e10965e4dd4a842c9d2cb778
Anne-Sophie Benoiston <AnneSoBen@users.noreply.github.com>
2024-03-28 08:55:40 +0100

    Update super_simple.sh

--- super_simple.sh
+++ super_simple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Count the number of sequence in the file F3D1_S189_L001_R1_001.fastq:"
+echo "Count the number of sequences in the file F3D1_S189_L001_R1_001.fastq:"
 
 nlines=`grep -c "^+$" ../../data/MiSeq_SOP/fastq_files/F3D1_S189_L001_R1_001.fastq`
 
